b8a282128e03b7bcd0009daeb2a10f8c462b57eb
Tim Chosen <timchosen@gmail.com>
2023-06-26 06:24:04 +0100

    added hooks

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,12 +2,33 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import ResetPassword from "./containers/ResetPassword";
+import ChangePassword from "./containers/ChangePassword";
 
 export default function Links() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
             <Route path="*" element={<NotFound />} />;
+            <Route
+                path="/login/reset"
+                // element={
+                //     <UnauthenticatedRoute>
+                //         <ResetPassword />
+                //     </UnauthenticatedRoute>
+                // }
+                element={
+                    <ResetPassword />
+                }
+            />
+            <Route
+                path="/settings/password"
+                element={
+                    <ChangePassword />
+                }
+            />
         </Routes>
     );
 }
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Home.css";
+import Button from "react-bootstrap/esm/Button";
+
 
 export default function Home() {
+    const cpatureDetails = () => {
+        console.log(navigator.userAgent);
+    }
     return (
         <div className="Home">
             <div className="lander">
                 <h1>Scratch</h1>
                 <p className="text-muted">A simple note taking app</p>
+                <Button onClick={cpatureDetails} >Click ME</Button>
             </div>
+
         </div>
     );
 }
